ee935a2c54eb3533c65da8c7a087d656827ebe33
jere-mie <jere-mie@users.noreply.github.com>
2020-12-31 16:28:14 -0500

    fake auth start

--- script.js
+++
@@ -1,8 +0,0 @@
-function run(){
-  var id = document.getElementById("uwonid").value;
-  var password = document.getElementById("password").value;
-  if (id=="ziad"){
-    alert("KOBTI!!!");
-  }
-
-}
